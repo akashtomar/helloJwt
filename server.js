dde237d21e5fd78a7f41b7139b6b3f99ab7e1184
akashtomar <akashtomar1995@yahoo.com>
2018-06-14 21:56:42 +0530

    express router and ejs begin

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express');
-
 var app = express();
+app.set('view engine', 'ejs');
+var router = require('./routes/sample')
+
+app.use('/',router);
 
 
-app.get('/', (req,res)=>{
-    console.log("git is working i think");
-    res.send('HELLO WORLD!11');
-});
+app.use(express.static('public'));
 
 
 app.listen(3000,()=>{
